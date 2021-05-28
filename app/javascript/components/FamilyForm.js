3512fc68b0f21147e2faeb959d0e56c1d5dd9f47
landon hickman <landonhickman12@gmail.com>
2021-05-28 17:44:55 -0600

    add family works

---
+++ app/javascript/components/FamilyForm.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import React, {useState} from 'react'
+
+const FamilyForm = (props) => {
+  const {id, addFamilyName} = props
+  const [name, setName] = useState(props.name ? props.name : '')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      if(id){
+        let res = await axios.put(`/families/${id}`, name)
+
+      }else {
+        let res = await axios.post('/families', {name})
+        addFamilyName(res.data)
+      }
+    } catch(err) {
+      alert(err)
+      console.log(err)
+    }
+    setName('')
+  }
+
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <p>{id ? 'Edit' : 'Add'} Last Name</p>
+        <input value={name} onChange={(e)=>setName(e.target.value)}/>
+        <button>{id ? 'Edit' : 'Add' }</button>
+      </form>
+    </div>
+  )
+}
+export default FamilyForm
--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Family from './Family'
+import FamilyForm from './FamilyForm'
 
 const Main = () => {
   const [families, setFamilies] = useState([])
@@ -17,6 +18,11 @@ const Main = () => {
     setFamilies(res.data)
   }
 
+  const addFamilyName = (familyName) => {
+    let createName = [familyName, ...families]
+    setFamilies(createName)
+  } 
+
   const renderFamilies = () => {
     if(families.length == 0){
       return <p>No Families!</p>
@@ -32,6 +38,7 @@ const Main = () => {
   return(
     <div>
       <h1>Main</h1>
+      <FamilyForm addFamilyName={addFamilyName}/>
       {renderFamilies()}
     </div>
   )
